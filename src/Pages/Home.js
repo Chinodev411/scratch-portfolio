167c80211969013a0484d02aa3d3cc3a3504f805
Chinodev411 <Chinodev@outlook.com>
2021-07-05 06:23:33 -0400

    production-progress

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,11 +3,13 @@ import portfolio from '../Images/portfolio.png';
 import '../App.css';
 import { SiLinkedin } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
+import { BiCopyright } from "react-icons/bi";
 
 const Home = () => {
   return (
     <div className="HomeDivContainer">
       <div className="Div2Home">
+      
           <img src={portfolio} alt="profile" className="image-pic" />
 
       </div>
@@ -17,15 +19,24 @@ const Home = () => {
       <div className="RightDivHome">
 
         <div className="LinkDiv"> 
-            <a href="https://www.linkedin.com/in/chinonso-ogbennia/" target="_blank" className="SideDivA"><SiLinkedin  className="FaLink"/></a>
+            <a href="https://www.linkedin.com/in/chinonso-ogbennia/" target="_blank" rel="noreferrer" className="SideDivA"><SiLinkedin  className="FaLink"/></a>
         </div>
   
 
         <div className="GitDiv">
-          <a href="https://github.com/Chinodev411" target="_blank" className="SideDivA"><FaGithub className="FaLink"/></a>
+          <a href="https://github.com/Chinodev411" target="_blank" rel="noreferrer" className="SideDivA"><FaGithub className="FaLink"/></a>
 
         </div> 
 
+      </div>
+      <div className="Intro-Div">
+        <h3 className="H-Main-Text"> CHINONSO ANSELEM OGBENNIA</h3>
+      </div>
+      
+      <div className="Footer-Div">
+      
+      <h4 className="Footer-Text"><BiCopyright className="Copy-Right"/>Copyright 2021 </h4> 
+      <h4 className="Footer-Text-2">All rights reserved. Created By ChinoTheRobot </h4>
       </div>
       
     </div>
